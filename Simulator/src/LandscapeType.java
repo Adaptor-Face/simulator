92888d1c1d86e2d691111a7a51d568935f7cdb15
Kristoffer <Kristoffer@158.38.92.121>
2017-02-23 10:21:03 +0100

    fhisdbfaklf

--- Simulator/src/LandscapeType.java
+++ Simulator/src/LandscapeType.java
@@ -15,9 +15,9 @@ public interface LandscapeType {
     public static String SHALLOWS = "SHALLOWS";
     public static String OCEAN = "OCEAN";
     public static double LAND_FOOD_DENSITY = 0.05;
-    public static double SHORE_FOOD_DENSITY = 0.15;
-    public static double SHALLOWS_FOOD_DENSITY = 0.4;
-    public static double OCEAN_FOOD_DENSITY = 0.7;
+    public static double SHORE_FOOD_DENSITY = 0.2;
+    public static double SHALLOWS_FOOD_DENSITY = 0.45;
+    public static double OCEAN_FOOD_DENSITY = 0.765;
     
     public void setType(String typeId);
     public String getType();
